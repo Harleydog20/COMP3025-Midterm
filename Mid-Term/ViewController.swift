516bba1f2549e200e5ec63f41a46ad8829ab3b67
student <student@students-mac-2.student.georgianc.on.ca>
2016-02-26 11:03:03 -0500

    Done layout - v.2

--- Mid-Term/ViewController.swift
+++ Mid-Term/ViewController.swift
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func less(sender: AnyObject) {
+    }
 
+    @IBAction func more(sender: AnyObject) {
+    }
 }
 
